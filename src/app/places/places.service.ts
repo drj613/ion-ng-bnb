0e8f19b3c98399ff68e448212e84952b805dcdc2
DJ <30848200+drj613@users.noreply.github.com>
2022-10-04 18:42:34 -0400

    setup routing, deleted unused home component, added a model and service for places

---
+++ src/app/places/places.service.ts
@@ -0,0 +1,39 @@
+/* eslint-disable no-underscore-dangle */
+import { Injectable } from '@angular/core';
+import { Place } from './place.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+  private _places: Place[] = [
+    new Place(
+      'p1',
+      'Manhattan Mansion',
+      'In the heart of New York City!',
+      'https://uncrate.com/p/2022/03/jonah-hill-loft-1.jpg',
+      149.99
+    ),
+    new Place(
+      'p2',
+      'L\'Amour Tojours',
+      'Romantic Parisian Getaway',
+      'https://d1dzqwexhp5ztx.cloudfront.net/imageRepo/4/0/81/939/245/5EssenceSuiteSite_O.jpg',
+      199.99
+    ),
+    new Place(
+      'p3',
+      'The Foggy Palace',
+      'Not your average city trip!',
+      'https://www.worldatlas.com/r/w1200/upload/c8/d7/68/shutterstock-121582312.jpg',
+      99.99
+    )
+  ];
+
+  constructor() { }
+
+  get places() {
+    return [...this._places];
+  }
+
+}
