cb70e3eabffe3fad8884dec43a7c9b399a5e02c6
DJ <30848200+drj613@users.noreply.github.com>
2022-10-06 10:20:41 -0400

    clickable places

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-place-detail',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private navCtrl: NavController) { }
 
   ngOnInit() {
   }
 
+  onBookPlace() {
+    // this.router.navigateByUrl('/places/tabs/discover');
+      // above doesn't always use the right animation
+      // we want to use the "backward" animation, so we should use the navCtrl instead
+    this.navCtrl.navigateBack('/places/tabs/discover');
+    // this.navCtrl.pop();
+      // .pop() removes current page from the top of the stack
+      // won't work if you reload on this page since stack will be 1 page total
+      // it's best for dynamic content that shouldn't be present on reload (like a modal)
+  }
 }
