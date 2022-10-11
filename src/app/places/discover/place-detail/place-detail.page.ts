91064c87892fc28bc5f529aa57490ca2a310539d
DJ <30848200+drj613@users.noreply.github.com>
2022-10-11 12:42:26 -0400

    handling user input

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -70,7 +70,8 @@ export class PlaceDetailPage implements OnInit {
     this.modalCtrl.create({
       component: CreateBookingModalComponent,
       componentProps: {
-        selectedPlace: this.place
+        selectedPlace: this.place,
+        selectedMode: mode
       }
     }).then(modalEl => {
       modalEl.present();
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -12,21 +12,27 @@ export class PlacesService {
       'Manhattan Mansion',
       'In the heart of New York City!',
       'https://uncrate.com/p/2022/03/jonah-hill-loft-1.jpg',
-      149.99
+      149.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p2',
       'L\'Amour Tojours',
       'Romantic Parisian Getaway',
       'https://d1dzqwexhp5ztx.cloudfront.net/imageRepo/4/0/81/939/245/5EssenceSuiteSite_O.jpg',
-      199.99
+      199.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     ),
     new Place(
       'p3',
       'The Foggy Palace',
       'Not your average city trip!',
       'https://www.worldatlas.com/r/w1200/upload/c8/d7/68/shutterstock-121582312.jpg',
-      99.99
+      99.99,
+      new Date('2019-01-01'),
+      new Date('2019-12-31')
     )
   ];
 
